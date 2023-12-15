91ce1695471bf662a5a2d6a1df8499bd2edb4d88
Abdullahh00 <125583431+Abdullahh00@users.noreply.github.com>
2023-12-15 09:17:04 +0500

    Update app.py part 2

--- doctors/app.py
+++ doctors/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 doctors = [
   { 'id': "100",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
-  { 'id': "20",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
+  { 'id': "200",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
 ]
 
 @app.route('/hello')
